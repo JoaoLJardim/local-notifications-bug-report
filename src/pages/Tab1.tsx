a1cd02bb73d6142c14b9950b12db5c8aac670b4d
João Jardim <jluisjardim1@gmail.com>
2022-12-13 14:52:01 +0000

    Add files via upload

---
+++ src/pages/Tab1.tsx
@@ -0,0 +1,109 @@
+import { LocalNotifications } from "@capacitor/local-notifications";
+import { DeliveredNotificationSchema } from "@capacitor/local-notifications/dist/esm/definitions";
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React, { useCallback, useState } from "react";
+
+import "./Tab1.css";
+
+const Tab1: React.FC = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const [message, setMessage] = useState<string[]>([]);
+  const [currentNotifications, setCurrentNotifications] = useState<
+    DeliveredNotificationSchema[]
+  >([]);
+
+  const updateNotificationList = useCallback(() => {
+    LocalNotifications.getDeliveredNotifications().then(({ notifications }) => {
+      setCurrentNotifications(notifications);
+    });
+  }, []);
+
+  const handlerAddNotification = useCallback(() => {
+    const nextNotificationId = currentId + 1;
+
+    LocalNotifications.schedule({
+      notifications: [
+        {
+          id: nextNotificationId,
+          title: `${nextNotificationId} notification`,
+          body: "",
+        },
+      ],
+    }).then(() => {
+      setMessage((v) => [...v, `Added ${nextNotificationId} notification`]);
+      updateNotificationList();
+    });
+
+    setCurrentId((v) => v + 1);
+  }, [currentId, updateNotificationList]);
+
+  const handlerNotificationDelete = useCallback(
+    (id: number) => {
+      LocalNotifications.getDeliveredNotifications().then(
+        ({ notifications }) => {
+          const notificationToDelete = notifications.filter(
+            (not) => not.id === id
+          );
+
+          notificationToDelete.forEach((not) => {
+            setMessage((v) => [...v, `Removed ${not.id} notification`]);
+          });
+
+          LocalNotifications.removeDeliveredNotifications({
+            notifications: notificationToDelete,
+          }).then(() => {
+            updateNotificationList();
+          });
+        }
+      );
+    },
+    [updateNotificationList]
+  );
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Local Notifications</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonButton onClick={handlerAddNotification}>
+          Add new notification
+        </IonButton>
+
+        <IonList>
+          <IonLabel>This is the list of notifications</IonLabel>
+          {currentNotifications.map((ionic, index) => (
+            <IonItem key={index}>
+              <IonLabel> {ionic.id}</IonLabel>
+              <IonButton onClick={() => handlerNotificationDelete(ionic.id)}>
+                Delete
+              </IonButton>
+            </IonItem>
+          ))}
+        </IonList>
+        <IonList>
+          <IonLabel>Messages</IonLabel>
+          {message.map((message) => (
+            <IonItem>
+              <IonLabel>{message}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
